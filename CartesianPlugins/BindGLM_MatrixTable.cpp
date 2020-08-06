6daa0422e350a494079abb000bf1b7cba1389130
liuyangping <liuyangping207@qq.com>
2020-08-06 17:50:58 +0800

    begin matrix table operation

---
+++ CartesianPlugins/BindGLM_MatrixTable.cpp
@@ -0,0 +1,20 @@
+//
+// Created by admin on 2020/8/6.
+//
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include "BindGLM_MatrixTable.h"
+namespace Cartesian{
+
+    template <int DIM>
+    sol::table mat_mul_mat(const sol::table &mat1, const sol::table &mat2){
+
+    }
+
+
+    void BindGLM_MatrixTable::bind(sol::state *state) {
+        // matrix_table * matrix_table -> matrix_table
+
+    }
+}
