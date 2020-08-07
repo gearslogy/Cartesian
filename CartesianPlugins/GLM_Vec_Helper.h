b97b391d55963ddb674a28a94827632725998595
liuyangping <liuyangping207@qq.com>
2020-08-07 17:33:46 +0800

    add vec to table functions

--- CartesianPlugins/GLM_Vec_Helper.h
+++ CartesianPlugins/GLM_Vec_Helper.h
@@ -9,6 +9,7 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include <vector>
 namespace Cartesian{
     class GLM_Vec_Helper{
     public:
@@ -24,6 +25,29 @@ namespace Cartesian{
                     table.get<float>(2),
                     table.get<float>(3),
                     table.get<float>(4));
+        }
+
+        static inline auto vec2_to_table(const glm::vec2 &vec){
+            std::vector<float> values;
+            values.emplace_back(vec.x);
+            values.emplace_back(vec.y);
+            return sol::as_table(values);
+        }
+
+        static inline auto vec3_to_table(const glm::vec3 &vec){
+            std::vector<float> values;
+            values.emplace_back(vec.x);
+            values.emplace_back(vec.y);
+            values.emplace_back(vec.z);
+            return sol::as_table(values);
+        }
+        static inline auto vec4_to_table(const glm::vec4 &vec){
+            std::vector<float> values;
+            values.emplace_back(vec.x);
+            values.emplace_back(vec.y);
+            values.emplace_back(vec.z);
+            values.emplace_back(vec.w);
+            return sol::as_table(values);
         }
     };
 
