28e4e34de031c77a93caecd06ad0105f36472375
liuyangping <liuyangping207@qq.com>
2020-08-08 21:44:08 +0800

    init cgal header

--- CartesianPlugins/BindCGAL.cpp
+++ CartesianPlugins/BindCGAL.cpp
@@ -3,3 +3,26 @@
 //
 
 #include "BindCGAL.h"
+
+
+#define CGAL_NO_GMP 1
+
+#include <vector>
+#include <CGAL/Simple_cartesian.h>
+#include <CGAL/Surface_mesh.h>
+
+typedef CGAL::Simple_cartesian<double> K;
+typedef CGAL::Surface_mesh<K::Point_3> Mesh;
+typedef Mesh::Vertex_index vertex_descriptor;
+typedef Mesh::Face_index face_descriptor;
+
+
+
+
+void Cartesian::BindCGAL::bind(sol::state *lua) {
+
+
+
+
+
+}
