c686c8458225ca361e7028c63ac2c908e9776416
liuyangping <liuyangping207@qq.com>
2020-08-10 19:59:59 +0800

    Face funciton init

---
+++ CartesianPlugins/CGAL_Bind/BindCGAL_Face_Functions.cpp
@@ -0,0 +1,16 @@
+
+#include "BindCGAL_Face_Functions.h"
+
+namespace Cartesian {
+
+
+	void BindCGAL_Face_Functions::bind(sol::state* lua)
+	{
+
+
+
+
+	}
+
+}
+
---
+++ CartesianPlugins/CGAL_Bind/BindCGAL_Face_Functions.h
@@ -0,0 +1,15 @@
+// 8/10/2020
+
+#pragma once
+#include "BindCGAL_DefineType.h"
+#include "sol.hpp"
+
+namespace Cartesian {
+
+	class BindCGAL_Face_Functions {
+	public:
+		static void bind(sol::state* lua);
+	};
+
+
+}
