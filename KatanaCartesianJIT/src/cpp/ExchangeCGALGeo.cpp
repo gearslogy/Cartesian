943759052e1d16d96ce14b668fd054ffa133041b
liuyangping <liuyangping207@qq.com>
2020-08-28 20:33:33 +0800

    geometry back to katana

--- KatanaCartesianJIT/src/cpp/ExchangeCGALGeo.cpp
+++ KatanaCartesianJIT/src/cpp/ExchangeCGALGeo.cpp
@@ -87,28 +87,28 @@ namespace Cartesian {
                 if (valueType == 1) { // it's int attribute
                     FnAttribute::IntAttribute refValueAttrib = static_cast<FnAttribute::IntAttribute> (valueAttrib);
                     auto tupleSize = refValueAttrib.getTupleSize();
-                    CARTESIAN_CORE_INFO("gen attribute map :{0}, type: {1}, tuple size: {2}, scope:{3}", attribName, "int", tupleSize, scope);
+                    CARTESIAN_CORE_INFO("gen attribute map: {0}, type: {1}, tuple size: {2}, scope: {3}", attribName, "int", tupleSize, scope);
                     MAKE_ATTRIB_MAP(attribName, scope, 1, valueAttrib, tupleSize);
                     katanaAttribMaps.emplace_back(ATTRIB_MAP);
                 }
                 if (valueType == 2) { // it's float attribute
                     FnAttribute::FloatAttribute refValueAttrib = static_cast<FnAttribute::FloatAttribute> (valueAttrib);
                     auto tupleSize = refValueAttrib.getTupleSize();
-                    CARTESIAN_CORE_INFO("gen attribute map:{0}, type: {1}, tuple size: {2}, scope:{3}", attribName, "float", tupleSize, scope);
+                    CARTESIAN_CORE_INFO("gen attribute map: {0}, type: {1}, tuple size: {2}, scope: {3}", attribName, "float", tupleSize, scope);
                     MAKE_ATTRIB_MAP(attribName, scope, 2, valueAttrib, tupleSize);
                     katanaAttribMaps.emplace_back(ATTRIB_MAP);
                 }
                 if (valueType == 3) { // it's double attribute
                     FnAttribute::DoubleAttribute refValueAttrib = static_cast<FnAttribute::DoubleAttribute> (valueAttrib);
                     auto tupleSize = refValueAttrib.getTupleSize();
-                    CARTESIAN_CORE_INFO("gen attribute map :{0}, type: {1}, tuple size: {2}, scope:{3}", attribName, "double", tupleSize, scope);
+                    CARTESIAN_CORE_INFO("gen attribute map: {0}, type: {1}, tuple size: {2}, scope: {3}", attribName, "double", tupleSize, scope);
                     MAKE_ATTRIB_MAP(attribName, scope, 3, valueAttrib, tupleSize);
                     katanaAttribMaps.emplace_back(ATTRIB_MAP);
                 }
                 if (valueType == 4) { // it's string attribute
                     FnAttribute::StringAttribute refValueAttrib = static_cast<FnAttribute::StringAttribute> (valueAttrib);
                     auto tupleSize = refValueAttrib.getTupleSize();
-                    CARTESIAN_CORE_INFO("gen attribute map :{0}, type: {1}, tuple size: {2}, scope:{3}", attribName, "string", tupleSize, scope);
+                    CARTESIAN_CORE_INFO("gen attribute map: {0}, type: {1}, tuple size: {2}, scope: {3}", attribName, "string", tupleSize, scope);
                     MAKE_ATTRIB_MAP(attribName, scope, 4, valueAttrib, tupleSize);
                     katanaAttribMaps.emplace_back(ATTRIB_MAP);
                 }
@@ -187,7 +187,7 @@ namespace Cartesian {
 
             if (attribmap.scope == "face")
             {
-               
+
                 switch (attribmap.type)
                 {
                 case 1:  // ----------------------------- int for face ---------------------------------------------------
@@ -225,6 +225,115 @@ namespace Cartesian {
 
     void SurfaceMeshToKatana(const PRE_TYPE::Mesh& mesh, Foundry::Katana::GeolibCookInterface& iface)
     {
+        const std::string polyStartIndexAttName = "geometry.poly.startIndex";
+        const std::string polyPolyVertexIndexAttName = "geometry.poly.vertexList";
+        const std::string pointPosAttName = "geometry.point.P";
+        const std::string arbitraryAttName = "geometry.arbitrary";
+
+        int numVertices = mesh.num_vertices();
+
+
+        // ------------------ construct geometry.point.P attribute -----------------------------
+        PRE_TYPE::Mesh::Property_map<PRE_TYPE::Vertex_descriptor, PRE_TYPE::K::Point_3> location = mesh.points();
+        int index = 0;
+        auto* rawPosdate = new float[numVertices * 3];
+        for (PRE_TYPE::Vertex_descriptor vd : mesh.vertices()) {
+            auto cgalPos = location[vd];
+            rawPosdate[index * 3 + 0] = cgalPos.x();
+            rawPosdate[index * 3 + 1] = cgalPos.y();
+            rawPosdate[index * 3 + 2] = cgalPos.z();
+            index++;
+        }
+        FnAttribute::FloatAttribute attr(rawPosdate, numVertices * 3, 3);
+        iface.setAttr(pointPosAttName, attr, false);
+        delete []rawPosdate;
+
+        // ------------------ construct geometry.poly.startIndex attribute -----------------------------
+        // this actually is likely primpoints()
+
+
+
+
+        // point attribute
+        for (auto& name : mesh.properties<PRE_TYPE::Vertex_descriptor>())
+        {
+            PRE_TYPE::Mesh::Property_map<PRE_TYPE::Vertex_descriptor, int> attribMapInt;
+            bool found = false;
+            boost::tie(attribMapInt, found) = mesh.property_map<PRE_TYPE::Vertex_descriptor, int>(name);
+            if (found) { // int
+                int* rawdata = new int[numVertices];
+                for (auto i = 0; i < numVertices; i++) {
+                    rawdata[i] = attribMapInt[PRE_TYPE::Vertex_descriptor(i)];
+                }
+                std::string attribValueName = arbitraryAttName + "." +name + ".value";
+                FnAttribute::IntAttribute attr(rawdata, numVertices, 1);
+                iface.setAttr(attribValueName, attr, false);
+                delete[]rawdata;
+            }
+
+
+            found = false;
+            PRE_TYPE::Mesh::Property_map<PRE_TYPE::Vertex_descriptor, float> attribMapFloat;
+            boost::tie(attribMapFloat, found) = mesh.property_map<PRE_TYPE::Vertex_descriptor, float>(name);
+            if (found) { // float
+                float* rawdata = new float[numVertices];
+                for (auto i = 0; i < numVertices; i++) {
+                    rawdata[i] = attribMapFloat[PRE_TYPE::Vertex_descriptor(i)];
+                }
+                std::string attribValueName = arbitraryAttName + "." + name + ".value";
+                FnAttribute::FloatAttribute attr(rawdata, numVertices, 1);
+                iface.setAttr(attribValueName, attr, false);
+                delete[]rawdata;
+            }
+
+            found = false;
+            PRE_TYPE::Mesh::Property_map<PRE_TYPE::Vertex_descriptor, double> attribMapDouble;
+            boost::tie(attribMapDouble, found) = mesh.property_map<PRE_TYPE::Vertex_descriptor, int>(name);
+            if (found) { // double
+                double* rawdata = new double[numVertices];
+                for (auto i = 0; i < numVertices; i++) {
+                    rawdata[i] = attribMapDouble[PRE_TYPE::Vertex_descriptor(i)];
+                }
+                std::string attribValueName = arbitraryAttName + "." + name + ".value";
+                FnAttribute::DoubleAttribute attr(rawdata, numVertices, 1);
+                iface.setAttr(attribValueName, attr, false);
+                delete[]rawdata;
+            }
+
+            found = false;
+            PRE_TYPE::Mesh::Property_map<PRE_TYPE::Vertex_descriptor, std::string> attribMapString;
+            boost::tie(attribMapString, found) = mesh.property_map<PRE_TYPE::Vertex_descriptor, std::string>(name);
+            if (found) { // string
+                std::string* rawdata = new std::string[numVertices];
+                for (auto i = 0; i < numVertices; i++) {
+                    rawdata[i] = attribMapString[PRE_TYPE::Vertex_descriptor(i)];
+                }
+                std::string attribValueName = arbitraryAttName + "." + name + ".value";
+                FnAttribute::StringAttribute attr(rawdata, numVertices, 1);
+                iface.setAttr(attribValueName, attr, false);
+                delete[]rawdata;
+            }
+
+
+            found = false;
+            PRE_TYPE::Mesh::Property_map<PRE_TYPE::Vertex_descriptor, glm::vec3> Vec3AttribMap;
+            boost::tie(Vec3AttribMap, found) = mesh.property_map<PRE_TYPE::Vertex_descriptor, glm::vec3>(name);
+            if (found) { // vec3
+                float* rawdata = new float[numVertices * 3];
+                for (auto i = 0; i < numVertices; i++) {
+                    glm::vec3 value = Vec3AttribMap[PRE_TYPE::Vertex_descriptor(i)];
+                    rawdata[i * 3 + 0] = value.x;
+                    rawdata[i * 3 + 1] = value.y;
+                    rawdata[i * 3 + 2] = value.z;
+                }
+                std::string attribValueName = arbitraryAttName + "." + name + ".value";
+                FnAttribute::FloatAttribute attr(rawdata, numVertices, 1);
+                iface.setAttr(attribValueName, attr, false);
+                delete[]rawdata;
+            }
+        }
+
+
 
     }// end of surface mesh to katana
 
